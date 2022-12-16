945a358890cd30832a95835594196245f08f1e2f
Ashesh Pangma <asheshpangma@gmail.com>
2022-12-16 13:29:33 +0000

    day3 completed

---
+++ Day3/rvmyth/rvmyth.ip_user_files/ip/ila_0/ila_0_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
+-- Date        : Fri Dec 16 12:58:56 2022
+-- Host        : fpga-workshop-01 running 64-bit Ubuntu 18.04.6 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/asheshpangma/VSD-FPGA-Fabric-Design-and-Architecture/Day3/rvmyth/rvmyth.srcs/sources_1/ip/ila_0/ila_0_stub.vhdl
+-- Design      : ila_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcpg236-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity ila_0 is
+  Port ( 
+    clk : in STD_LOGIC;
+    probe0 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe1 : in STD_LOGIC_VECTOR ( 7 downto 0 )
+  );
+
+end ila_0;
+
+architecture stub of ila_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk,probe0[0:0],probe1[7:0]";
+attribute X_CORE_INFO : string;
+attribute X_CORE_INFO of stub : architecture is "ila,Vivado 2019.2";
+begin
+end;
