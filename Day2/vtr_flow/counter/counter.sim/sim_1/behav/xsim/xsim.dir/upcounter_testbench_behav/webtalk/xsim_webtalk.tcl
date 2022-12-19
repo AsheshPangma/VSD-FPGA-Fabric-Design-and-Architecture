fc6873aa6b26a69cb9d608c784a7bf149f9c014f
Ashesh Pangma <asheshpangma@gmail.com>
2022-12-19 19:39:01 +0000

    updated

--- Day2/vtr_flow/counter/counter.sim/sim_1/behav/xsim/xsim.dir/upcounter_testbench_behav/webtalk/xsim_webtalk.tcl
+++ Day2/vtr_flow/counter/counter.sim/sim_1/behav/xsim/xsim.dir/upcounter_testbench_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir /home/asheshpangma/VSD-FPGA-Fabric-Design-and-Architecture/Day2/vtr_flow/counter/counter.sim/sim_1/behav/xsim/xsim.dir/upcounter_testbench_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu Dec 15 20:29:35 2022" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Dec 19 19:06:09 2022" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2019.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2708876" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "LIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "3485b074-4d62-4ea8-8fb9-ea997b8b1f29" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "35a19cd2581b43a3a5bb87d4dac057fc" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "4" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "7" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Ubuntu" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "Ubuntu 18.04.6 LTS" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "DO-Premium-Intel" -context "user_environment"
@@ -22,11 +22,12 @@ webtalk_add_data -client project -key cpu_speed -value "2494.138 MHz" -context "
 webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "16.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
+webtalk_add_data -client xsim -key runall -value "true" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "0 ps" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "840790 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.02_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "3.44_sec" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Memory -value "126016_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 276172294 -regid "" -xml /home/asheshpangma/VSD-FPGA-Fabric-Design-and-Architecture/Day2/vtr_flow/counter/counter.sim/sim_1/behav/xsim/xsim.dir/upcounter_testbench_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/asheshpangma/VSD-FPGA-Fabric-Design-and-Architecture/Day2/vtr_flow/counter/counter.sim/sim_1/behav/xsim/xsim.dir/upcounter_testbench_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/asheshpangma/VSD-FPGA-Fabric-Design-and-Architecture/Day2/vtr_flow/counter/counter.sim/sim_1/behav/xsim/xsim.dir/upcounter_testbench_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_transmit -clientid 1452118032 -regid "" -xml /home/asheshpangma/VSD-FPGA-Fabric-Design-and-Architecture/Day2/vtr_flow/counter/counter.sim/sim_1/behav/xsim/xsim.dir/upcounter_testbench_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/asheshpangma/VSD-FPGA-Fabric-Design-and-Architecture/Day2/vtr_flow/counter/counter.sim/sim_1/behav/xsim/xsim.dir/upcounter_testbench_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/asheshpangma/VSD-FPGA-Fabric-Design-and-Architecture/Day2/vtr_flow/counter/counter.sim/sim_1/behav/xsim/xsim.dir/upcounter_testbench_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
